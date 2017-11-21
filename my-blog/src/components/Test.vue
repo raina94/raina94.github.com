2c81142904ff84713e4b524ba646e7c48365877f
Ryeong22 <ksr1027@ilts.co.kr>
2017-11-21 21:28:21 +0900

    일단커밋

--- my-blog/src/components/Test.vue
+++ my-blog/src/components/Test.vue
@@ -1,6 +1,9 @@
 <template>
-    <div>
-    {{msg}}
+    <div class="header">
+    <ul>
+        <li>김성령</li>
+        <li>안녕</li>
+    </ul>
     </div>
 </template>
 
@@ -14,3 +17,10 @@
         }
     }
 </script>
+
+<style scoped>
+    .header {
+        width: 100%;
+        height: 100px;
+    }
+</style>
